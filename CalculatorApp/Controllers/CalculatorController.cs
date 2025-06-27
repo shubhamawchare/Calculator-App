f8c3f50372a20957b2a4771366343974c9b25505
Shubham Awchare <shubhamawchare@gmail.com>
2025-06-27 19:33:25 +0530

    Final Commit

---
+++ CalculatorApp/Controllers/CalculatorController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CalculatorApp.Models;
+
+namespace CalculatorApp.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new CalculatorModel());
+        }
+
+        [HttpPost]
+        public IActionResult Index(CalculatorModel model)
+        {
+            switch (model.Operator)
+            {
+                case "Add":
+                    model.Result = model.Number1 + model.Number2;
+                    break;
+                case "Subtract":
+                    model.Result = model.Number1 - model.Number2;
+                    break;
+                case "Multiply":
+                    model.Result = model.Number1 * model.Number2;
+                    break;
+                case "Divide":
+                    model.Result = model.Number2 != 0 ? model.Number1 / model.Number2 : 0;
+                    break;
+            }
+
+            return View(model);
+        }
+    }
+}
